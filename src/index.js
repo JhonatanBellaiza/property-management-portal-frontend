467ad4504b8fbfe86259fbc18d525a275d464914
Jhonatan <jhonatan_bellaiza@hotmail.com>
2024-02-06 17:17:39 -0600

    Fixing changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AlertTemplate from "react-alert-template-basic";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
